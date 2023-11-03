2bfac9e22f823a16320b759bdbd843e16f742139
RiJo <102893295+yhrjfj@users.noreply.github.com>
2023-11-03 10:00:09 +0600

    Add TextField

--- app/src/main/java/com/yhr/jfj/jetnote/screen/NoteScreen.kt
+++ app/src/main/java/com/yhr/jfj/jetnote/screen/NoteScreen.kt
@@ -1,6 +1,11 @@
 package com.yhr.jfj.jetnote.screen
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
@@ -13,12 +18,19 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -45,6 +57,52 @@ fun NoteScreen() {
             )
         }
     ) {
+        Surface(
+            modifier = Modifier
+                .padding(
+                    start = 8.dp,
+                    top = 70.dp,
+                    end = 8.dp,
+                    bottom = 8.dp
+                )
+                .fillMaxSize()
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Divider(color = MaterialTheme.colorScheme.secondary)
 
+                // Column for title and description
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    // TextField for title
+                    var text by remember {
+                        mutableStateOf("")
+                    }
+                    TextField(value = text, onValueChange = {
+                        text = it
+                    },
+                        label = { Text(text = "Title") }
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    // TextField for description
+                    TextField(value = text, onValueChange = {
+                        text = it
+                    },
+                        label = { Text(text = "Description") })
+                }
+
+
+//
+//                // TextField for description
+//                TextField(value = , onValueChange = )
+            }
+
+        }
     }
 }
